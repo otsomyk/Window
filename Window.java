dd9f9713fee4de57eced7a3e75d6bc2c84459669
otsomyk <otsomyk@users.noreply.github.com>
2016-01-27 15:29:31 +0200

    Initial commit

---
+++ Window.java
@@ -0,0 +1,16 @@
+package window;
+
+import javax.swing.JFrame;
+
+public class Window {
+
+	public static void main(String[] args) {
+		 JFrame f = new JFrame();
+	        f.setLocation(300, 100);
+	        f.setSize(800, 600);
+	        f.setVisible(true);
+
+
+	}
+
+}
